2197d518b115fad8f8859c88d41cee7f788009a7
Woximus <wiktor.wlodarczyk006@gmail.com>
2026-05-04 20:16:41 +0200

    Inicjalizacja i podlaczenie z baza

---
+++ src/main/java/pl/Wlodarczyk/ogarnijsie/OgarnijsieApplication.java
@@ -0,0 +1,13 @@
+package pl.Wlodarczyk.ogarnijsie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OgarnijsieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OgarnijsieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/Wlodarczyk/ogarnijsie/OgarnijsieApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.Wlodarczyk.ogarnijsie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OgarnijsieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
